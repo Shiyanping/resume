841e0e03573d68107a5d1f4b8e7c4405960ad7c8
shiyanping <shiyanping@coohua.com>
2018-06-19 22:03:56 +0800

    修改简历

--- src/pages/front.vue
+++ src/pages/front.vue
@@ -2,7 +2,7 @@
   <div class="main-container" v-show="$parent.loading" style="display: none;">
     <div class="useful-link no-print">
       <!--<a class="theme-link" href="/index2.html"></a>-->
-      <a class="github-button" href="https://github.com/eternityspring/eternityspring.github.io" title="查看源码">
+      <a class="github-button" href="https://github.com/Shiyanping/resume" title="查看源码">
         <img src="/static/images/star.svg" alt="Github"><span>Star</span>
       </a>
     </div>
@@ -29,13 +29,13 @@
             </div>
             <div class="contact-info">
               <div class="row">
-                <div class="col-md-6 col-lg-3">
+                <div class="col-xs-6 col-md-6 col-lg-3">
                   <div class="item wow inShow" data-wow-delay="0.3s">
                     <h4>性别</h4>
                     <div class="info">{{userInfo.sex}}</div>
                   </div>
                 </div>
-                <div class="col-md-6 col-lg-3">
+                <div class="col-xs-6 col-md-6 col-lg-3">
                   <div class="item wow inShow" data-wow-delay="0.45s">
                     <h4>年龄</h4>
                     <div class="info">{{calcDate(userInfo.birthday)}}</div>
@@ -50,7 +50,7 @@
                 <div class="col-md-6 col-lg-3">
                   <a class="item wow inShow" data-wow-delay="0.55s" :href="userInfo.website" target="_blank">
                     <h4>经验</h4>
-                    <div class="info">{{userInfo.exp[0]+(calcDate(userInfo.workSince)-1)+userInfo.exp[1]+calcDate(userInfo.workSince)+userInfo.exp[2]}}</div>
+                    <div class="info">{{userInfo.exp[0]+(calcDate(userInfo.workSince)-1)+userInfo.exp[1]+(calcDate(userInfo.workSince)-0.5)+userInfo.exp[2]}}</div>
                   </a>
                 </div>
               </div>
@@ -94,17 +94,17 @@
             <h2 class="title">介绍&nbsp;/&nbsp;
               <small><i>Intro</i></small>
             </h2>
-            <div class="description">介绍一些个人基本情况</div>
+            <div class="description">个人基本情况</div>
           </div>
         </header>
         <div class="section-content">
           <div class="intro">
-            <p v-for="intro in userInfo.intro.content" v-html="intro"></p>
+            <p v-for="(intro, introIndex) in userInfo.intro.content" :key="introIndex" v-html="intro"></p>
           </div>
           <div class="technology">
             <ul class="inline">
               <li><b>{{userInfo.intro.technology.title}}</b></li>
-              <li v-for="item in userInfo.intro.technology.content">{{item}}</li>
+              <li v-for="(item, itemIndex) in userInfo.intro.technology.content" :key="itemIndex">{{item}}</li>
             </ul>
           </div>
         </div>
@@ -119,12 +119,12 @@
             <h2 class="title">工作经验&nbsp;/&nbsp;
               <small><i>Experience</i></small>
             </h2>
-            <div class="description">不同时期简短的从业经历介绍</div>
+            <div class="description">简短的从业经历介绍</div>
           </div>
         </header>
         <div class="section-content">
           <div class="experience">
-            <div class="item" v-for="item in userInfo.experience">
+            <div class="item" v-for="(item, index) in userInfo.experience" :key="index">
               <div class="row">
                 <div class="col-md-5">
                   <div class="time">{{item.time}}</div>
@@ -133,16 +133,16 @@
                 </div>
                 <div class="col-md-7">
                   <div class="content">{{item.description}}</div>
-                  <div class="tips" v-for="tip in item.tips">
+                  <div class="tips" v-for="(tip, tipIndex) in item.tips" :key="tipIndex">
                     <b>{{tip.title}}</b>
                     <ul>
-                      <li v-for="list in tip.content">{{list}}</li>
+                      <li v-for="(list, listIndex) in tip.content" :key="listIndex">{{list}}</li>
                     </ul>
                   </div>
                   <div class="technology" v-for="tech in item.technology">
                     <b>{{tech.title}}</b>
                     <ul class="inline">
-                      <li v-for="techItem in tech.content">{{techItem}}</li>
+                      <li v-for="(techItem, techItemIndex) in tech.content" :key="techItemIndex">{{techItem}}</li>
                     </ul>
                   </div>
                 </div>
@@ -166,7 +166,7 @@
         </header>
         <div class="section-content">
           <div class="row skill">
-            <div class="col-md-6" v-for="s in userInfo.skill">
+            <div class="col-md-6" v-for="(s, sIndex) in userInfo.skill" :key="sIndex">
               <div class="item">
                 <div class="text-info">
                   <span class="num text-light">{{s.percent}}</span>{{s.name}}
@@ -194,7 +194,7 @@
         </header>
         <div class="section-content">
           <div class="experience">
-            <div class="item" v-for="item in userInfo.project">
+            <div class="item" v-for="(item,index) in userInfo.project" :key="index">
               <div class="row">
                 <div class="col-md-5">
                   <div class="time">{{item.time}}</div>
@@ -203,16 +203,16 @@
                 </div>
                 <div class="col-md-7">
                   <div class="content">{{item.description}}</div>
-                  <div class="tips" v-for="tip in item.tips">
+                  <div class="tips" v-for="(tip, experienceTipIndex) in item.tips">
                     <b>{{tip.title}}</b>
                     <ul>
-                      <li v-for="list in tip.content">{{list}}</li>
+                      <li v-for="(list,listIndex) in tip.content" :key="listIndex">{{list}}</li>
                     </ul>
                   </div>
-                  <div class="technology" v-for="tech in item.technology">
+                  <div class="technology" v-for="(tech,techIndex) in item.technology" :key="techIndex">
                     <b>{{tech.title}}</b>
                     <ul class="inline">
-                      <li v-for="techItem in tech.content">{{techItem}}</li>
+                      <li v-for="(techItem,techItemIndex) in tech.content" :key="techItemIndex">{{techItem}}</li>
                     </ul>
                   </div>
                 </div>
@@ -231,12 +231,12 @@
             <h2 class="title">常逛&nbsp;/&nbsp;
               <small><i>Usually</i></small>
             </h2>
-            <div class="description">经常逛的技术或者设计相关网站</div>
+            <div class="description">经常逛的技术相关网站</div>
           </div>
         </header>
         <div class="section-content">
           <div class="row usually">
-            <div class="col-lg-4 col-md-6" v-for="u in userInfo.usually">
+            <div class="col-lg-4 col-md-6" v-for="(u,index) in userInfo.usually" :key="index">
               <a v-if="u.link" :href="u.link" target="_blank" class="item" :title="u.link">
                 <i class="fa fa-github"></i>
                 <h3 class="text-light">{{u.name}}</h3>
@@ -267,25 +267,25 @@
         <div class="section-content">
           <div class="contact">
             <div class="row">
-              <div class="col-md-6 col-lg-3">
+              <div class="col-md-6 col-lg-3 contact-bottom">
                 <div class="item">
                   <h4>Phone</h4>
                   <div class="info">{{userInfo.phone}}</div>
                 </div>
               </div>
-              <div class="col-md-6 col-lg-3">
+              <div class="col-md-6 col-lg-3 contact-bottom">
                 <div class="item">
                   <h4>QQ</h4>
                   <div class="info">{{userInfo.qq.number}}</div>
                 </div>
               </div>
-              <div class="col-md-6 col-lg-3">
+              <div class="col-md-6 col-lg-3 contact-bottom">
                 <div class="item">
                   <h4>Location</h4>
                   <div class="info">{{userInfo.location}}</div>
                 </div>
               </div>
-              <div class="col-md-6 col-lg-3">
+              <div class="col-md-6 col-lg-3 contact-bottom">
                 <a class="item" :href="'mailto:'+userInfo.email">
                   <h4>Email</h4>
                   <div class="info">{{userInfo.email}}</div>
@@ -350,7 +350,7 @@
       calcDate(birthday){
         let birthdayDate = new Date(birthday);
         let todyDate = new Date();
-        return todyDate.getYear() - birthdayDate.getYear()
+        return todyDate.getYear() - birthdayDate.getYear() + 1
       }
     }
   }
